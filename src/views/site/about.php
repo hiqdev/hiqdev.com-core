4b3f95131decc21b178594d7b2ec772b443f9450
tofid <andreyklochok@gmail.com>
2017-01-12 13:14:11 +0200

    Added base pages

---
+++ src/views/site/about.php
@@ -0,0 +1,22 @@
+---
+layout: page
+title: About
+---
+
+<p class="message">
+Twas brillig, and the slithy toves<br>
+Did gyre and gimble in the wabe;<br>
+All mimsy were the borogoves,<br>
+And the mome raths outgrabe.
+</p>
+
+We're doing [HiPanel](/hipanel), [HiDev](/hidev) and other [cool projects](/projects) and [packages](/packages).
+
+## Our amazing team
+
+ * [Sol](https://github.com/hiqsol)
+ * [SilverFire](https://github.com/SilverFire)
+ * [Tafid](https://github.com/tafid)
+ * [BladeRoot](https://github.com/bladeroot)
+
+This site is made with Dr. [Jekyll]({% post_url 2012-02-06-whats-jekyll %}) and Mr. [Hyde]({% post_url 2013-12-28-introducing-hyde %}).
---
+++ src/views/site/packages.php
@@ -0,0 +1,6 @@
+---
+layout: page
+title: Packages
+---
+
+## Here's going to be list of our packages.
