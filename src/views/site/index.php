ea2b8c81540b7cee89ea4d0e66ccf5ce5b0112e9
tofid <andreyklochok@gmail.com>
2017-01-13 19:58:22 +0200

    minior

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -5,57 +5,28 @@ use yii\helpers\Html;
 
 $this->title = Yii::$app->name;
 
-$dataProvider = new \yii\data\ArrayDataProvider([
-    'allModels' => [
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-        ['title' => 123, 'post-date' => '2017-01-12'],
-        ['title' => 456, 'post-date' => '2017-01-12'],
-    ],
-]);
-
 ?>
 
-<?= \yii\widgets\ListView::widget([
-    'layout' => "{items}\n{pager}",
-    'options' => [
-        'class' => 'posts',
-    ],
-    'itemOptions' => [
-        'class' => 'post',
-    ],
-    'dataProvider' => $dataProvider,
-    'pager' => [
-        'class' => LinkPager::class,
-        'prevPageLabel' => Yii::t('hiqdev:com', 'Newer'),
-        'nextPageLabel' => Yii::t('hiqdev:com', 'Older'),
-    ],
-    'itemView' => function ($model, $key, $item, $widget) {
-        $out = Html::tag('h1', $model['title'], ['class' => 'post-title']);
-        $out .= Html::tag('span', Yii::$app->formatter->asDate($model['post-date']), ['class' => 'post-date']);
+<?php if ($dataProvider) : ?>
+    <?= \yii\widgets\ListView::widget([
+        'layout' => "{items}\n{pager}",
+        'options' => [
+            'class' => 'posts',
+        ],
+        'itemOptions' => [
+            'class' => 'post',
+        ],
+        'dataProvider' => $dataProvider,
+        'pager' => [
+            'class' => LinkPager::class,
+            'prevPageLabel' => Yii::t('hiqdev:com', 'Newer'),
+            'nextPageLabel' => Yii::t('hiqdev:com', 'Older'),
+        ],
+        'itemView' => function ($model, $key, $item, $widget) {
+            $out = Html::tag('h1', Html::a($model['basename'], ['/pages/render/index', 'page' => $model['path']]), ['class' => 'post-title']);
+            $out .= Html::tag('span', Yii::$app->formatter->asDate($model['timestamp']), ['class' => 'post-date']);
 
-        return $out;
-    },
-]) ?>
+            return $out;
+        },
+    ]) ?>
+<?php endif ?>
